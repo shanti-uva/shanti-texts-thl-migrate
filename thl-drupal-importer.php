1559aa42f5f7aae396d136420cc51c49ddf9c42c
Raf Alvarado <ontoligent@gmail.com>
2015-01-16 12:00:58 -0500

    Updated Importer

--- thl-drupal-importer.php
+++ thl-drupal-importer.php
@@ -70,11 +70,15 @@ foreach ($filenames as $filename) {
           $author = '';
           if (is_object($author_inf)) {
             $author = $author_inf->fullname;
+            print "Author is an object ...\n";
           } elseif (is_array($author_inf)) {
             $author = $author_inf['fullname'];
+            print "Author is an array ...\n";
           } else {
+          print "Author is a string ...\n";
             $author = $author_inf;
           }
+          print "AUTHOR: $author\n";
           if ($author != $prev_author) {
             $node->field_book_author[$node->language][0]['value'] = $author;
           }
